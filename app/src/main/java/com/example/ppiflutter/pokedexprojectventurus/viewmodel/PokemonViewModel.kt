e536a5c2d19b67f0a05e01004b559f2dd7170290
GLennK999 <guilhermecossu@gmail.com>
2025-11-06 13:10:45 -0300

    Add PokemonViewModel structure

--- app/src/main/java/com/example/ppiflutter/pokedexprojectventurus/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/example/ppiflutter/pokedexprojectventurus/viewmodel/PokemonViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.ppiflutter.pokedexprojectventurus.viewmodel
 
-class PokemonViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.ppiflutter.pokedexprojectventurus.api.PokemonRepository
+import com.example.ppiflutter.pokedexprojectventurus.model.PokemonModel
+
+class PokemonViewModel: ViewModel() {
+
+    var pokemonlist = MutableLiveData<List<PokemonModel?>>()
+
+    init {
+        Thread({
+            loadPokemons()
+        }).start()
+    }
+
+    private fun loadPokemons(){
+        val pokemonApiResult= PokemonRepository.listPokemons()
+
+        pokemonApiResult?.results?.let{
+
+        }
+    }
+
 }
